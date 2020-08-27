6b0faf0edb9e00c6c1b3699dcbe860b4cc8e4a89
Raphael Freire <raphaelmachadofreire@gmail.com>
2020-08-26 22:59:13 -0300

    initial commit

---
+++ src/components/SectionHero/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import Logo from 'components/Logo'
+import Button from 'components/Button'
+import * as S from './styles'
+
+import { getImageUrl } from 'utils/getImageUrl'
+
+import { gaEvent } from 'utils/ga'
+import Container from 'components/Container'
+import { HeaderProps, LogoProps } from 'types/api'
+
+const onClick = () =>
+  gaEvent({ action: 'click', category: 'cta', label: 'hero button' })
+
+type Props = {
+  logo: LogoProps
+  header: HeaderProps
+}
+
+const SectionHero = ({ logo, header }) => (
+  <S.Wrapper>
+    <Container>
+      <Logo {...logo} />
+
+      <S.Content>
+        <S.TextBlock>
+          <S.Title>{header.title}</S.Title>
+          <S.Description>{header.description}</S.Description>
+          <S.ButtonWrapper>
+            <Button href={header.button.url} onClick={onClick} wide>
+              {header.button.label}
+            </Button>
+          </S.ButtonWrapper>
+        </S.TextBlock>
+
+        <S.Image
+          src={getImageUrl(header.image.url)}
+          alt={header.image.alternativeText}
+        />
+      </S.Content>
+    </Container>
+  </S.Wrapper>
+)
+
+export default SectionHero
